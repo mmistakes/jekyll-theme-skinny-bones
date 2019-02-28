e344eade2116c1d5ca5b3b9a866f0c5f0d110089
Cristian Román <cromanpa94@gmail.com>
2019-02-28 08:35:28 -0700

    Update _main.js

--- js/_main.js
+++ js/_main.js
@@ -1,14 +1,14 @@
 // Off Canvas Sliding
-//$(document).ready(function(){
+$(document).ready(function(){
   // Menu button click
- // $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
-    // $('#js-body').toggleClass('no-scroll');
+  $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
+     $('#js-body').toggleClass('no-scroll');
  //   $('#js-menu, #js-menu-screen').toggleClass('is-visible');
- //   $('#js-menu-trigger').toggleClass('slide close');
-    // $('#masthead, #page-wrapper').toggleClass('slide');
- //   e.preventDefault();
- // });
-//});
+    $('#js-menu-trigger').toggleClass('slide close');
+     $('#masthead, #page-wrapper').toggleClass('slide');
+    e.preventDefault();
+  });
+});
 
 // FitVids
 $(document).ready(function(){
