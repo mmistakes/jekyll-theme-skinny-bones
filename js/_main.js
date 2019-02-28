e8034b771b63dbf7a14de6597ae76075b7226deb
Cristian Román <cromanpa94@gmail.com>
2019-02-28 08:42:15 -0700

    Update _main.js

--- js/_main.js
+++ js/_main.js
@@ -2,10 +2,10 @@
 $(document).ready(function(){
   // Menu button click
   $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
-     $('#js-body').toggleClass('no-scroll');
- //   $('#js-menu, #js-menu-screen').toggleClass('is-visible');
+    // $('#js-body').toggleClass('no-scroll');
+    $('#js-menu, #js-menu-screen').toggleClass('is-visible');
     $('#js-menu-trigger').toggleClass('slide close');
-     $('#masthead, #page-wrapper').toggleClass('slide');
+    // $('#masthead, #page-wrapper').toggleClass('slide');
     e.preventDefault();
   });
 });
@@ -17,4 +17,4 @@ $(document).ready(function(){
 });
 
 // Table of Contents title. Change text to localize
-//$("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>");
+$("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>");
